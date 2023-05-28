de340e43928b48f03be3b4ff15a61ade2e93921a
joabecoelho <contatojoabe@icloud.com>
2023-05-28 18:48:19 -0300

    Ajustes de bug

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
+const Instrument = require('../models/Instrument');
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 //helpers
@@ -10,72 +11,73 @@ const getUserByToken = require('../helpers/get-user-by-token')
 
 
 module.exports = class UserController {
-    static async register(req, res){
-       const { name, email, phone, password, confirmpassword } = req.body
-
-       //validations
-       if (!name) {
-          res.status(422).json({message: "O nome é obrigatório."})
-          return
-       }
-
-       if (!email) {
-          res.status(422).json({message: "O email é obrigatório."})
-          return
-       }
-
-       if (!phone) {
-        res.status(422).json({message: "O telefone é obrigatório."})
-        return
-     }
-
-     if (!password) {
-        res.status(422).json({message: "A senha é obrigatória."})
-        return
-     }
-
-     if (!confirmpassword) {
-        res.status(422).json({message: "A confirmação de senha é obrigatória."})
-        return
-     }
-
-     if(password !== confirmpassword){
-        res.status(400).json({message: "A senha e a confirmação da senha precisam ser iguais."})
-     }
-
-     // check if user exists
-     const userExists = await User.findOne({email: email})
-
-     if(userExists) {
+   static async register(req, res) {
+      const { name, email, phone, password, confirmpassword } = req.body;
+    
+      // validations
+      if (!name) {
+        res.status(422).json({ message: "O nome é obrigatório." });
+        return;
+      }
+    
+      if (!email) {
+        res.status(422).json({ message: "O email é obrigatório." });
+        return;
+      }
+    
+      if (!phone) {
+        res.status(422).json({ message: "O telefone é obrigatório." });
+        return;
+      }
+    
+      if (!password) {
+        res.status(422).json({ message: "A senha é obrigatória." });
+        return;
+      }
+    
+      if (!confirmpassword) {
+        res.status(422).json({ message: "A confirmação de senha é obrigatória." });
+        return;
+      }
+    
+      if (password !== confirmpassword) {
         res
-        .status(422)
-        .json({
-            message: "Email já cadastrado!"
-        })
-        return
-     }
-
-     // create password
-     const salt = await bcrypt.genSalt(12)
-     const passwordHash = await bcrypt.hash(password, salt)
-
-     // create user
-     const user = new User({
+          .status(400)
+          .json({ message: "A senha e a confirmação da senha precisam ser iguais." });
+        return;
+      }
+    
+      // check if user exists
+      const userExists = await User.findOne({ email: email });
+    
+      if (userExists) {
+        res.status(422).json({
+          message: "Email já cadastrado!"
+        });
+        return;
+      }
+    
+      // create password
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+    
+      // create user
+      const user = new User({
         name,
         email,
         phone,
-        password: passwordHash,
-     })
-
-     try {
-        const newUser = await user.save()
-
-        await createUserToken(newUser, req, res)
-        
-     } catch (error) {
-        res.status(500).json({message: error})
-     }
+        password: passwordHash
+      });
+    
+      try {
+        const newUser = await user.save();
+    
+        await createUserToken(newUser, req, res);
+      } catch (error) {
+        res.status(500).json({ message: error });
+      }
     }
+    
 
     static async login(req, res) {
 
@@ -210,12 +212,17 @@ module.exports = class UserController {
     }
 
    try {
-       // returns user pudate data
+       // returns user update data
        const updateUser =  await User.findByIdAndUpdate(
          {_id: user._id},
          {$set: user},
          {new: true},
        )
+
+       await Instrument.updateMany(
+         { 'user._id': user._id },
+         { $set: { 'user': updateUser } }
+       );
 
        res.status(200).json({
          message: "Usuário atualizado com sucesso!",
