2c75aee19f507a9cf2cab934f9221f5eeffe76da
joabecoelho <contatojoabe@icloud.com>
2023-05-28 18:52:33 -0300

    Ajustes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 const Instrument = require('../models/Instrument');
 
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 //helpers
