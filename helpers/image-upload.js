f6be8d17becfeeb0f73263743de061b9d71c5c8e
joabecoelho <contatojoabe@icloud.com>
2023-05-28 21:40:09 -0300

    Removendo backblaze-b2

--- helpers/image-upload.js
+++ helpers/image-upload.js
@@ -1,48 +1,39 @@
-const multer = require('multer');
-const B2 = require('backblaze-b2');
+const multer = require('multer')
+const path = require('path')
 
-// Configurar as credenciais do Backblaze B2
-const b2 = new B2({
-  accountId: '0050cca9e29dc970000000001',
-  applicationKey: 'K005g9wavfezz+lh9F9w79rHJT2Z0rM',
-});
-
-// Autenticar com o Backblaze B2
-b2.authorize().then(() => {
-  console.log('Autenticação bem-sucedida com o Backblaze B2');
-}).catch((err) => {
-  console.error('Erro na autenticação com o Backblaze B2:', err);
-});
-
-// Configurar o armazenamento do multer com o Backblaze B2
+// Destination to store the images
 const imageStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    let folder = '';
+    destination: function( req, file, cb) {
 
-    if (req.baseUrl.includes('users')) {
-      folder = 'users';
-    } else if (req.baseUrl.includes('instruments')) {
-      folder = 'instruments';
-    }
+        let folder = ""
 
-    cb(null, `public/images/${folder}`);
-  },
-  filename: function (req, file, cb) {
-    const filename =
-      Date.now() + String(Math.floor(Math.random() * 1000)) + path.extname(file.originalname);
-    cb(null, filename);
-  },
-});
+        if(req.baseUrl.includes("users")) {
+            folder ="users"
+        } else if (req.baseUrl.includes("instruments")) {
+            folder = "instruments"
+        }
 
-// Criar uma instância do multer com o armazenamento no Backblaze B2
+        cb(null, `public/images/${folder}`)
+
+    },
+    filename: function(req, file, cb) {
+        cb(
+            null, 
+            Date.now() + 
+            String(Math.floor(Math.random() * 1000)) + 
+            path.extname(file.originalname),
+            )
+    },
+}) 
+
 const imageUpload = multer({
-  storage: imageStorage,
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
-      return cb(new Error('Por favor, envie apenas arquivos jpg ou png!'));
-    }
-    cb(undefined, true);
-  },
-});
+    storage: imageStorage,
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb(new Error("Por favor, envie apenas arquivos jpg ou png!"))
+        }
+        cb(undefined, true)
+    },
+})
 
-module.exports = { imageUpload };
+module.exports = { imageUpload }
